84cf384e9cc06c4743e4af8a6be36d954ff88e4d
Huỳnh Tấn Chương <tanchuonghuynh3@gmail.com>
2024-11-04 22:16:22 +0700

    QuanLyCuaHang_DeA

---
+++ Kiem_Tra/QuanLyCuaHang_DeA/QuanLyCuaHang_DeA/CuaHang.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyCuaHang_DeA
+{
+    internal class CuaHang
+    {
+        private List<HoaDon> danhSachHoaDon = new List<HoaDon>();
+
+        public CuaHang()
+        {
+        }
+
+        public CuaHang(List<HoaDon> danhSachHoaDon)
+        {
+            this.DanhSachHoaDon = danhSachHoaDon;
+        }
+
+        internal List<HoaDon> DanhSachHoaDon { get => danhSachHoaDon; set => danhSachHoaDon = value; }
+
+        public void Nhap()
+        {
+            Console.InputEncoding = Encoding.UTF8;
+            Console.OutputEncoding = Encoding.UTF8;
+
+            int soHoaDon = 1;
+
+            while (true)
+            {
+                HoaDon hoaDon = new HoaDon();
+                hoaDon.Nhap(soHoaDon++);
+                DanhSachHoaDon.Add(hoaDon);
+
+                Console.Write("Bạn có muốn nhập thêm hóa đơn không? (Y/N): ");
+                string response = Console.ReadLine().Trim().ToUpper();
+                if (response != "Y") break;
+            }
+        }
+
+        public void Xuat()
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            int tongDoanhThu = 0;
+
+            foreach (HoaDon hoaDon in DanhSachHoaDon)
+            {
+                hoaDon.Xuat();
+                tongDoanhThu += hoaDon.TinhTongTriGia();
+            }
+
+            Console.WriteLine($"\nTong doanh thu cua cua hang: {tongDoanhThu}");
+        }
+    }
+}
---
+++ Kiem_Tra/QuanLyCuaHang_DeA/QuanLyCuaHang_DeA/MatHang.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyCuaHang_DeA
+{
+    internal class MatHang
+    {
+        private int maHang;
+        private string tenHang;
+        private int soLuong;
+        private int donGia;
+        private int thanhTien;
+
+        public MatHang()
+        {
+        }
+
+        public MatHang(int maHang, string tenHang, int soLuong, int donGia, int thanhTien)
+        {
+            this.MaHang = maHang;
+            this.TenHang = tenHang;
+            this.SoLuong = soLuong;
+            this.DonGia = donGia;
+            this.ThanhTien = thanhTien;
+        }
+
+        public int MaHang 
+        { 
+            get => maHang; 
+            set => maHang = value; 
+        }
+
+        public string TenHang 
+        {
+            get => tenHang; 
+            set => tenHang = value; 
+        }
+
+        public int SoLuong 
+        { 
+            get => soLuong; 
+            set => soLuong = value; 
+        }
+
+        public int DonGia 
+        { 
+            get => donGia; 
+            set => donGia = value; 
+        }
+
+        public int ThanhTien 
+        { 
+            get => thanhTien; 
+            set => thanhTien = value; 
+        }
+
+        public void Nhap()
+        {
+            Console.InputEncoding = Encoding.UTF8;
+            Console.OutputEncoding = Encoding.UTF8;
+
+            Console.Write("Nhập mã hàng: ");
+            while (!int.TryParse(Console.ReadLine(), out maHang) || maHang <= 0)
+            {
+                Console.Write("Mã hàng phải là số nguyên dương. Nhập lại mã hàng: ");
+            }
+
+            Console.Write("Nhập tên hàng: ");
+            tenHang = Console.ReadLine();
+
+            Console.Write("Nhập số lượng: ");
+            while (!int.TryParse(Console.ReadLine(), out soLuong) || soLuong <= 0)
+            {
+                Console.Write("Số lượng phải là số nguyên dương. Nhập lại số lượng: ");
+            }
+
+            Console.Write("Nhập đơn giá: ");
+            while (!int.TryParse(Console.ReadLine(), out donGia) || donGia <= 0)
+            {
+                Console.Write("Đơn giá phải là số nguyên dương. Nhập lại đơn giá: ");
+            }
+
+            ThanhTien = SoLuong * DonGia;
+        }
+
+        public void Xuat(int stt)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine($"{stt,-5} {MaHang,-10} {TenHang,-15} {SoLuong,-10} {DonGia,-10} {ThanhTien,-10}");
+        }
+    }
+}
